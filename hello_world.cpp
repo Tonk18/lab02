734c91576aeec4093df02ea51c25dcc89ab497e0
Tonk18 <charkovskya@gmail.com>
2025-04-26 17:13:24 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@
 #include <string>
 
 int main() {
-    std::string name;  // Variable to store user name
+    std::string name;  // Variable to store user name!!!!!!!
     std::cout << "Enter your name: ";
     std::cin >> name;
     std::cout << "Hello world from " << name << std::endl;
